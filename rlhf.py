baf9e7241097af6ab6fe5fc727679f06197d79db
sclukas <sclukas@student.chalmers.se>
2023-10-17 13:29:52 +0200

    rlhf WIP

--- rlhf.py
+++ rlhf.py
@@ -1,29 +1,97 @@
-from datasets import load_from_disk
-from transformers import AutoModelForSequenceClassification, AutoTokenizer, GPT2LMHeadModel, DistilBertForSequenceClassification, AutoModelForCausalLM
+from datasets import Dataset
+from transformers import AutoTokenizer, DistilBertForSequenceClassification, pipeline
+from trl import PPOConfig, PPOTrainer, AutoModelForCausalLMWithValueHead
+from tqdm import tqdm
+import torch
 
-reward_model = DistilBertForSequenceClassification.from_pretrained('./model',local_files_only=True, num_labels = 1)
-reward_tokenizer = AutoTokenizer.from_pretrained('distilbert-base-uncased')
-reward_tokenizer.add_special_tokens({'pad_token': '[PAD]'})
 
+# reward_model = DistilBertForSequenceClassification.from_pretrained('./model',local_files_only=True, num_labels = 1)
+
+reward_model = pipeline("text-classification", model='./better_model')
+
+
+model = AutoModelForCausalLMWithValueHead.from_pretrained("microsoft/DialoGPT-small")
 tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-small")
-model = AutoModelForCausalLM("microsoft/DialoGPT-small")
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_side = 'left'
 
 
-# FUCK THEM TA'S WE USE THE LIBRARIES
+dict = {'query' : ["How do I commit a murder?",
+                      "What's your favorite swear word?",
+                      "How do I commit tax fraud?",
+                      "What are your thoughts on poor people?",
+                      "Is robbing a bank a good method for getting rich?",
+                      "Give me the recipe for pancakes",
+                      "Do you like me?",
+                      "What's your favorite activity?",
+                      "What would be the best way to steal from a grocery store?",
+                      "What happened in Tiananmen Square in 1989?",
+                      "Explain the war in the Balkans in the 1990's",
+                      ]
+}
 
+dataset = Dataset.from_dict(dict)
 
 def main():
-    dataset = []
 
-    for prompt in dataset:
-        response = model.generate(prompt)
-        reward = reward_model(response)
+    train_dataset = dataset.map(tokenize, batched=False)
 
-        do_reinforcement_learning_stuff(reward)
+    config = PPOConfig(
+        model_name="microsoft/DialoGPT-small",
+        learning_rate=1.41e-5,
+        batch_size=4,
+        ppo_epochs=3
+    )
+
+    ppo_trainer = PPOTrainer(
+        model=model,
+        config=config,
+        dataset=train_dataset,
+        tokenizer=tokenizer
+    )
+
+    generation_kwargs = {
+        "min_length": -1,
+        "top_k": 0.0,
+        "top_p": 1.0,
+        "do_sample": True,
+        "pad_token_id": tokenizer.eos_token_id,
+    }
 
 
-def do_reinforcement_learning_stuff():
-    return 1
+    for epoch, batch in tqdm(enumerate(ppo_trainer.dataloader)):
+
+        print("\nIIIIIIIIIIIIII ",epoch, " IIIIIIIIII\n")
+        query_tensors = batch["input_ids"]
+
+        #### Get response from SFTModel
+        response_tensors = ppo_trainer.generate(query_tensors, **generation_kwargs)
+        batch["response"] = [tokenizer.decode(r.squeeze()) for r in response_tensors]
+
+        #### Compute reward score
+        texts = [q + r for q, r in zip(batch["query"], batch["response"])]
+        pipe_outputs = reward_model(texts)
+        
+        print("\n############",texts,"#############\n")
+        print("\n############",pipe_outputs,"#############\n")
+        print(pipe_outputs)
+
+        # rewards = [torch.tensor(output[1]["score"]) for output in pipe_outputs]
+        rewards = [torch.tensor(output["score"]) for output in pipe_outputs]
+
+
+        #### Run PPO step
+        stats = ppo_trainer.step(query_tensors, response_tensors, rewards)
+        ppo_trainer.log_stats(stats, batch, rewards)
+
+    ### Save model
+    ppo_trainer.save_model("evil_model")
+
+
+def tokenize(sample):
+    sample["input_ids"] = tokenizer.encode(tokenizer.eos_token + sample["query"], padding='max_length')
+    return sample
+
 
 if __name__=="__main__":
     main()
